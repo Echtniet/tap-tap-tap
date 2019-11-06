0592ffde0f8bf4c8c89f2e54a20e9708c0eff233
clinton davelaar <davelaarprogramming@gmail.com>
2019-11-06 12:27:25 -0600

    Initial Commit

---
+++ tap tap tap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tap tap tap
+//
+//  Created by Davelaar,Clinton B on 11/6/19.
+//  Copyright © 2019 Davelaar,Clinton B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
